9841f6875f11ac5dbe7eb67d3f54672c1293ab4a
Zach Richling <zwrdude@gmail.com>
2025-06-22 13:43:08 -0500

    Update to production bot Emoji ids

--- PuddleBot/Extensions/LavalinkTrackExtensions.cs
+++ PuddleBot/Extensions/LavalinkTrackExtensions.cs
@@ -8,12 +8,13 @@ namespace PuddleBot.Extensions
         public static string IconTitleTime(this LavalinkTrack track) => $"{GetSourceIcon(track)} {track.Title}. ({track.Duration:hh\\:mm\\:ss})";
         private static string GetSourceIcon(LavalinkTrack track) => track.SourceName switch
         { 
-            "youtube" => "<:Youtube:1386204946546036737>",
-            "soundcloud" => "<:Soundcloud:1386205241934086154>",
-            "bandcamp" => "<:Bandcamp:1386205694222536755>",
-            "twitch" => "<:Twitch:1386205704381267968>",
-            "applemusic" => "<:AppleMusic:1386204975415300166>",
-            _ => "<:Youtube:1386204946546036737>" // Default to YouTube icon for unsupported sources
+            "youtube" => "<:Youtube:1386415534400344094>",
+            "soundcloud" => "<:Soundcloud:1386415494462177280>",
+            "bandcamp" => "<:Bandcamp:1386415447796350996>",
+            "twitch" => "<:Twitch:1386415430318686449>",
+            "applemusic" => "<:AppleMusic:1386415518386229390>",
+            "vimeo" => "<:Vimeo:1386415443081957396>",
+            _ => "<:Youtube:1386415534400344094>" // Default to YouTube icon for unsupported sources
         };
         
     }
